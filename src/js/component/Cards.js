07ea0b2e8e51df38cf6fa5a3cf3bc39b7d24338e
brcre001 <brandon.f.creed@gmail.com>
2021-07-25 17:30:46 +0000

    added solid or outline when favorite button is clicked

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Card, Button } from "react-bootstrap";
 import { propTypes } from "react-bootstrap/esm/Image";
 
 export const Cards = props => {
+	const [favorite, setFavorite] = useState(true);
+
 	return (
-		<Card style={{ width: "18rem" }} className="col-2 m-3">
+		<Card style={{ width: "18rem" }} className="col-2 m-3 p-0">
 			<Card.Img variant="top" src="holder.js/400px200" />
 			<Card.Body>
 				<Card.Title>{props.title}</Card.Title>
 				<Card.Text>{props.body}</Card.Text>
 				<Button variant="outline-primary">Learn More!</Button>
-				<Button variant="outline-warning" className="float-right">
-					<i className="far fa-heart" />
+				<Button variant="outline-warning" className="float-right" onClick={() => setFavorite(!favorite)}>
+					{favorite ? <i className="far fa-heart" /> : <i className="fas fa-heart" />}
 				</Button>
 			</Card.Body>
 		</Card>
